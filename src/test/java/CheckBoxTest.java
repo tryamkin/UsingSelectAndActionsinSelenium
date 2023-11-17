2d501dd511034c0f230765c7361f99aa39b398df
tryam <tryamkin@gmail.com>
2023-11-17 12:42:02 +0200

    checkboxes

--- src/test/java/CheckBoxTest.java
+++ src/test/java/CheckBoxTest.java
@@ -3,32 +3,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.sql.Time;
 import java.time.Duration;
 
 public class CheckBoxTest {
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void firstTEst() throws InterruptedException {
+    public void CheckTest() throws InterruptedException {
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         WebElement checkBox1 = driver.findElement(By.xpath("//input[@id='my-check-1']"));
         WebElement checKBox2 = driver.findElement(By.xpath("//input[@id='my-check-2']"));
         checkBox1.click();
+        checKBox2.click();
         Thread.sleep(1000);
-
-
-        checkBox1.click();    Thread.sleep(1000);
-        checkBox1.click();   Thread.sleep(1000);
-
-
-
-        WebElement message = driver.findElement(By.id("message"));
-        System.out.println(message.getText());
+        Assert.assertFalse(checkBox1.isSelected());
+        checkBox1.click();
+        checKBox2.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(checkBox1.isSelected());
+        checkBox1.click();
+        checKBox2.click();
+        Thread.sleep(1000);
+        checkBox1.click();
+        checKBox2.click();
+        Thread.sleep(1000);
         driver.quit();
     }
 }
